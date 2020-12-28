24504bf4186f10b56d037b6ef9cca89853ef27b6
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2020-12-29 02:24:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("#partnersSlider").slick({
+	  dots: false,
+	  autoplay:true,
+	  autoplaySpeed:3000,
+	  arrows:true,
+	  infinite: true,
+	  speed: 2000,
+	  slidesToShow: 6,
+	  slidesToScroll: 1
+	});
+});
