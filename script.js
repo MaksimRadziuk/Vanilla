4b7a82a1566ff3e2d4a4914db885209983ae6454
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2020-12-30 03:18:59 +0300

    30.12 3.18

--- script.js
+++ script.js
@@ -9,4 +9,27 @@ $(document).ready(function(){
 	  slidesToShow: 6,
 	  slidesToScroll: 1
 	});
+	$(".feedback_slider-wrap").slick({
+	  dots: false,
+	  autoplay:true,
+	  autoplaySpeed:3000,
+	  arrows:true,
+	  infinite: true,
+	  speed: 2000,
+	  slidesToShow: 5,
+	  slidesToScroll: 1
+	});
+	$(".article_slider-wrap").slick({
+	  dots: false,
+	  autoplay:false,
+	  arrows:true,
+	  infinite: true,
+	  speed: 400,
+	  slidesToShow: 4,
+	  slidesToScroll: 1
+	});
+
+	$('.faq_block .question').click(function(){
+		$(this).next('.answer').slideToggle("slow");
+	});
 });
